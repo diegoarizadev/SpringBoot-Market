de826341450d9e66e5cf6d3bfc643be845e558b2
Diego Ariza <diego.ariza2@gmail.com>
2022-10-02 14:26:06 -0500

    Query Methods y Query clasicoas

--- src/main/java/com/tplinkdns/hagakur3/market/persistence/ProductRepository.java
+++ src/main/java/com/tplinkdns/hagakur3/market/persistence/ProductRepository.java
@@ -4,11 +4,28 @@ import com.tplinkdns.hagakur3.market.persistence.crud.ProductoCrudRepository;
 import com.tplinkdns.hagakur3.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.finByIdCategoria(idCategoria);
+    }
+
+    public List<Producto> getByCategoriaOrderByNombreASC(int idCategoria){
+        return productoCrudRepository.finByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public List<Producto> getByCategoriaNoMethods(int idCategoria){
+        return productoCrudRepository.getIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> getProductosEscasos(int cantidad){
+        return productoCrudRepository.finByIdCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/tplinkdns/hagakur3/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/tplinkdns/hagakur3/market/persistence/crud/ProductoCrudRepository.java
@@ -1,10 +1,22 @@
 package com.tplinkdns.hagakur3.market.persistence.crud;
 
 import com.tplinkdns.hagakur3.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> { //CrudRepository <Table o clase, Tipo de dato de la PK>
 
+    List<Producto> finByIdCategoria(int idCategoria); // QUERY METHOD, busca todos los productos por IdCategoria
 
 
+    List<Producto> finByIdCategoriaOrderByNombreAsc(int idCategoria); //QUERY METHOD, busca y ordena los productos por id de categoria
+
+    Optional<List<Producto>> finByIdCantidadStockLessThanAndEstado(int cantidadStock, boolean estado); //QUERY METHOD, recuperar productos escasos y que mas se venden
+
+
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getIdCategoria(int idCategoria); //Construcción de una consulta SIN QUERY METHOD
 }
