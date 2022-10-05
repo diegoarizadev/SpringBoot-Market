6ce1d65e4d876a85aa0f539067f8c22c88040359
Diego <diego.ariza2@gmail.com>
2022-10-04 21:46:11 -0500

    Se crean los MApeos necesarios para el dominio de compras.

---
+++ src/main/java/com/tplinkdns/hagakur3/market/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,29 @@
+package com.tplinkdns.hagakur3.market.persistence.mapper;
+
+import com.tplinkdns.hagakur3.market.domain.PurchaseItem;
+import com.tplinkdns.hagakur3.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class}) //Objetivo es poder ser inyectable desde otras clases o implementaciones
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            //@Mapping(source = "total", target = "total"), //no se agrega porque el source es igual al mapper
+            @Mapping(source = "estado", target = "active")
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration //Esta conversión hace le mappinmg inverso del objeto anterior.
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true)
+    })
+    ComprasProducto toComprasProducto(PurchaseItem item);
+
+}
